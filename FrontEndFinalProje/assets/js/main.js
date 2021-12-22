fe57e07d1b7ddd73f20be7492270f7b967d77ac5
Aygunaab <aygunaab@code.edu.az>
2021-12-22 11:15:32 -0500

    banner

---
+++ FrontEndFinalProje/assets/js/main.js
@@ -0,0 +1,13 @@
+
+$('.slider-banner').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 3000,
+ prevArrow:".arrow-prev",
+ nextArrow:".arrow-right",
+  
+});
